cb0169339deb7cdeaf3402654893069272917dc2
Joao Martins <up201806436@fe.up.pt>
2021-03-08 17:45:11 +0000

    Fix: Achievments are more responsive

--- html/pages/profile.php
+++ html/pages/profile.php
@@ -193,153 +193,164 @@ drawHeader($title);
           <h6><b>Achivements</b></h6>
         </div>
         <div class="container d-flex flex-wrap justify-content-center mt-3">
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>First Question</b></h6>
                 <p class="text-start fs-7 p-0">Make your first question</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
-                <h6 class="text-start"><b>20 Upvotes</b></h6>
-                <p class="text-start fs-7 p-0">Get 20 upvotes on a question or answer</p>
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
+                <h6 class=""><b>20 Upvotes</b></h6>
+                <p class="fs-7 p-0">Get 20 upvotes on a question or answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>Top answer</b></h6>
                 <p class="text-start fs-7 p-0">Get your answer chosen by the owner of a question</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
-                <h6 class="text-start"><b>50 Score</b></h6>
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
+                <h6 class=""><b>50 Upvotes</b></h6>
                 <p class="text-start fs-7 p-0">Get 50 score points</p>
             </div>
           </div>
 
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>First Comment</b></h6>
                 <p class="text-start fs-7 p-0">Make your first comment</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>50 Upvotes</b></h6>
                 <p class="text-start fs-7 p-0">Get 20 upvotes on a question or answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>5 Top answers</b></h6>
                 <p class="text-start fs-7 p-0">Get 5 of your answer chosen by the owner of a question</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>100 Score</b></h6>
                 <p class="text-start fs-7 p-0">Get 100 score points</p>
             </div>
           </div>
 
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border border-success rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>First Answer</b></h6>
                 <p class="text-start fs-7 p-0">Make your first answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>100 Upvotes</b></h6>
                 <p class="text-start fs-7 p-0">Get 100 upvotes on a question or answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>15 Top answers</b></h6>
                 <p class="text-start fs-7 p-0">Get 15 of your answer chosen by the owner of a question</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>200 Score</b></h6>
                 <p class="text-start fs-7 p-0">Get 200 score points</p>
             </div>
           </div>
 
-          <div class="container-fluid row-1 border rounded border-success m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+          <div class="container-fluid row align-items-center border border-success rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>First Vote</b></h6>
                 <p class="text-start fs-7 p-0">Make your first vote on a question or answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>200 Upvotes</b></h6>
                 <p class="text-start fs-7 p-0">Get 200 upvotes on a question or answer</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded border-success m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>30 Top answers</b></h6>
                 <p class="text-start fs-7 p-0">Get 30 of your answer chosen by the owner of a question</p>
             </div>
           </div>
-          <div class="container-fluid row-1 border rounded m-3 m-sm-1 p-1" style="max-width: 20em;">
-            <div class="col-auto text-start mt-3 p-0">
-              <img src="/static/images/trophy.jpg" style="float: left;" class="rounded" width="60" alt="trophy-logo">
+
+          <div class="container-fluid row align-items-center border rounded m-1 p-1 bg-dark" style="max-width: 20em;">
+            <div class="col-3 text-start ps-2">
+              <img src="/static/images/trophy.jpg" class="rounded achievement-logo" width="100%" alt="trophy-logo">
             </div>
-            <div class="col-auto align-middle mt-4 me-0 p-0">
+            <div class="col-9 col-xs-7 align-middle text-start text-wrap lh-sm">
                 <h6 class="text-start"><b>450 Score</b></h6>
                 <p class="text-start fs-7 p-0">Get 450 score points</p>
             </div>
           </div>
+
         </div>
       </div>
     </div>
