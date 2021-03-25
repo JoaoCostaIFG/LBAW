0d68c657e38ef2449e8793b49fd898b1e339c1a6
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-03-25 23:12:40 +0000

    Add: trigger for user reputation.

--- database/triggers.sql
+++ database/triggers.sql
@@ -26,6 +26,31 @@ AS $$
     END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION reputation()
+RETURNS TRIGGER
+AS $$
+    DECLARE
+        val integer;
+        owner_id integer;
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            val := -OLD.score;
+            owner_id := OLD.id_owner;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            val := -OLD.score + NEW.score;
+            owner_id := NEW.id_owner;
+        ELSIF (TG_OP = 'INSERT') THEN
+            val := NEW.value;
+            owner_id := NEW.id_owner;
+        END IF;
+        
+        UPDATE "user"
+        SET score = score + val
+        WHERE id = owner_id;
+
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+$$ LANGUAGE plpgsql;
 
 -- TRIGGERS
 DROP TRIGGER IF EXISTS update_score ON vote CASCADE;
@@ -33,3 +58,9 @@ CREATE TRIGGER update_score
 AFTER DELETE OR INSERT OR UPDATE
 ON vote
 FOR EACH ROW EXECUTE FUNCTION score();
+
+DROP TRIGGER IF EXISTS update_reputation ON post CASCADE;
+CREATE TRIGGER update_reputation
+AFTER DELETE OR INSERT OR UPDATE
+OF score ON post
+FOR EACH ROW EXECUTE FUNCTION reputation();
