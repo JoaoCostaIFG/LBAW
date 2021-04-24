87034bcd73ad2012e934029db38de391eb18a32f
Ana Ines <anaiobarros2014@gmail.com>
2021-04-24 23:00:14 +0100

    Add: question card blade

--- app/Http/Controllers/SearchResultsController.php
+++ app/Http/Controllers/SearchResultsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Question;
 
 use Illuminate\Http\Request;
 
@@ -13,8 +14,18 @@ class SearchResultsController extends Controller
     }
 
     public function show(){
-        // TODO: Send users and posts from search
-        $users = User::all();
-        return view("pages.search_results", ['users' => $users]);
+        // TODO: Send users from search
+        $users = User::all(); // Replace this
+        // TODO: Send questions from search
+        /*
+         Questions missing: 
+          - question number of answers
+          - post body (limited number of characters)
+          - question tags
+          - number of days since post
+          - owner pic
+        */
+        $questions = Question::all(); // Replace this
+        return view("pages.search_results", ['users' => $users, 'question' => $questions]);
     }
 }
---
+++ resources/views/partials/search_results/question_card.blade.php
@@ -0,0 +1,52 @@
+<!-- Question Card -->
+<div class="d-flex flex-sm-row flex-column-reverse justify-content-start align-items-center gap-3 w-100 border p-3">
+  <div class="d-flex flex-sm-column flex-row justify-content-start align-items-center gap-1 ">
+    <!-- Number Votes -->
+    <div class="d-flex flex-sm-column flex-row justify-content-center align-items-center gap-1">
+      <span class="fs-4">{{ $question->score }}</span>
+      <span>Votes</span>
+      <span class="d-sm-none"><i class="bi bi-arrow-down-up"></i></span>
+    </div>
+    <!-- Number Answers -->
+    <div class="d-flex  flex-sm-column flex-row justify-content-center align-items-center gap-1">
+      <span class="fs-4">{{ $question->num_answers }}</span>
+      <span>Answers</span>
+      <span class="d-sm-none"><i class="bi bi-chat-left-text"></i></span>
+    </div>
+  </div>
+  <div class="d-flex flex-column justify-content-start align-items-start w-100">
+    <section>
+      <!-- Question Title -->
+      <a class="fs-5 fw-bold" href="/pages/question_admin.php">{{ $question->title }}</a>
+      <!-- Question body -->
+      <p class="d-none d-md-block">{{ $question->post->body }}</p>
+    </section>
+    <div class="w-100 gap-1 d-flex flex-row align-items-center justify-content-between align-self-start">
+      <!-- Question Tags -->
+      <div class="d-none d-sm-block d-grid gap-1 mt-0">
+        @foreach ($question->tags as $tag)
+          <a class="badge bg-primary text-decoration-none">{{ $tag }}</a>
+        @endforeach
+      </div>
+      <!-- Question Date -->
+      <div class="d-flex flex-row gap-2 align-items-center">
+        <span class="align-self-end">Posted {{ $question->days }} days ago</span>
+        <!-- Author User Info -->
+        <!-- User Profile Pic  -->
+        <img class="d-none d-md-block rounded-2 fit-cover" src="../static/images/user.png" alt="User Picture" width="40px" height="40px">
+        <div class="d-flex flex-row flex-md-column justify-content-center align-items-start">
+          <!-- Owner Username -->
+          <span class="d-md-none">by&nbsp;</span>
+          <a href="/pages/profile.php">
+            {{ $question->owner->username }}
+          </a>
+          <!-- Owner Reputation -->
+          <span class="d-none d-md-block">
+            {{ $question->owner->reputation }}
+            <i class="bi bi-award"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
