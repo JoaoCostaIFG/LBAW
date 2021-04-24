69d81f6869d1c79035e3a396dbbf7dd03b2eefef
ricardofontao2000 <up201806317@fe.up.pt>
2021-04-24 19:56:38 +0100

    Refactored leaderboard page

---
+++ resources/views/partials/leaderboard/leaderboard_user_entry.blade.php
@@ -0,0 +1,11 @@
+<tr>
+    <th scope="row" class="text-center">
+        {{ $place }}
+    </th>
+    <td class="text-center">
+        {{ $user->username }}
+    </td>
+    <td class="text-center">
+        {{$user->reputation}}
+    </td>
+  </tr>
