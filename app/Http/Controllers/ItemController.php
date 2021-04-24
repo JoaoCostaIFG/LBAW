84aaa96dc13e4a012f413a272d0ab1806b37dbb0
ricardofontao2000 <up201806317@fe.up.pt>
2021-04-24 20:00:03 +0100

    Cleanup unused code

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
-use App\Models\Card;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
