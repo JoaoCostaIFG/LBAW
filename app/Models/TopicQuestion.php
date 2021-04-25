61b3c9448b01c4675a0ce78d270971a656dccb6c
Ana Ines <anaiobarros2014@gmail.com>
2021-04-25 17:59:04 +0100

    Topic Model

---
+++ app/Models/TopicQuestion.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TopicQuestion extends Model
+{
+    use HasFactory;
+
+    protected $table = "topic_question";
+
+    // Don't add create and update timestamps in database.
+    public $timestamps = false;
+
+
+    public function question()
+    {
+        return $this->hasOne(Question::class, 'id', 'id_question');
+    }
+
+    public function topic()
+    {
+        return $this->hasOne(Topic::class, 'id', 'id_topic');
+    }
+}
