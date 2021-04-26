3443403dc493c6c895a4d004aa38586db13b1f43
ricardofontao2000 <up201806317@fe.up.pt>
2021-04-26 13:19:10 +0100

    Small fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Policies\QuestionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -15,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
     //   'App\Models\Card' => 'App\Policies\CardPolicy',
     //   'App\Models\Item' => 'App\Policies\ItemPolicy'
-        'App\Models\Question' => 'App\Policies\QuestionPolicy'
+        Question::class => QuestionPolicy::class
     ];
 
     /**
