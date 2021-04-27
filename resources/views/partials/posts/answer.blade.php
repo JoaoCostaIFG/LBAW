a8736b7cbb0bbfeebccee327704b6216b5adc2ea
ricardofontao2000 <up201806317@fe.up.pt>
2021-04-27 09:44:28 +0100

    Added accpet answer button

--- resources/views/partials/posts/answer.blade.php
+++ resources/views/partials/posts/answer.blade.php
@@ -17,10 +17,13 @@
     </div>
 
     <div class="row">
+        @if (Auth::id() == $answer->question->post->owner->id && is_null($answer->question->accepted_answer))
+            <button type="button" class="btn btn-success col-sm-2 offset-sm-7 col-4 offset-5 my-2">Mark accepted</button>
+        @endif
         <button type="button" class="btn btn-danger col-sm-2 offset-sm-7 col-4 offset-5 my-2">Report</button>
         <p class="text-muted col-3 text-center m-0 align-self-center">Posted {{ (new DateTime($answer->post->date))->diff(new DateTime('NOW'))->days }} day ago</p>
     </div>
 
-    @include('partials.posts.comment_block', ['post' => $answer]) {{-- Maybe needs to receive post and not answer/question --}}
+    @include('partials.posts.comment_block', ['post' => $answer])
 
 </div>
