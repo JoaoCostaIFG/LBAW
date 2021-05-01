9fa1acde44ec33268b5e075ee450b55a55ae4482
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-01 16:57:50 +0100

    Update: Auth form bg colors and positioning.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Register')
 
 @section('content')
-<form class="container-md bg-primary p-3" method="POST" action="{{ route('register') }}">
+<form class="container-md auth-form py-3 px-5" method="POST" action="{{ route('register') }}">
     {{ csrf_field() }}
     <!-- Email -->
     <div class="input-group mb-3">
