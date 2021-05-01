5eedfb73d4a7badc23ba6203d7396be55bd95eae
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-01 17:08:33 +0100

    More icons :D

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Login')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
+<form class="container-sm authform" method="POST" action="{{ route('login') }}">
   {{ csrf_field() }}
   <!-- Username -->
   <div class="input-group mb-3">
-    <label for="username" class="form-label"><b>Username</b><span class="text-danger"> *</span></label>
+    <label for="username" class="form-label"><i class="bi bi-person-fill"></i> <b>Username</b> <span class="text-danger">*</span></label>
     <div class="input-group has-validation">
       <input id="username" type="text" class="form-control" name="username" placeholder="Enter your username..." value="{{ old('username') }}" required autofocus>
       @if ($errors->has('username'))
@@ -19,7 +19,7 @@
   </div>
   <!-- Password -->
   <div class="input-group mb-3">
-    <label for="password" class="form-label"><b>Password</b><span class="text-danger"> *</span></label>
+    <label for="password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Password</b> <span class="text-danger">*</span></label>
     <div class="input-group has-validation">
       <input id="password" type="password" class="form-control" name="password" placeholder="Enter password..." required>
       @if ($errors->has('password'))
