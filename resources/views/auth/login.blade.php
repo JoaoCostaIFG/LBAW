aaff8fe38fc630028ddf3e4dca66e2aa3281d649
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-01 19:09:42 +0100

    Update: auth pages css.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,33 +3,43 @@
 @section('title', 'Login')
 
 @section('content')
-<form class="auth-form" method="POST" action="{{ route('login') }}">
-  {{ csrf_field() }}
-  <!-- Username -->
-  <div class="input-group mb-3">
-    <label for="username" class="form-label"><i class="bi bi-person-fill"></i> <b>Username</b> <span class="text-danger">*</span></label>
-    <div class="input-group has-validation">
-      <input id="username" type="text" class="form-control" name="username" placeholder="Enter your username..." value="{{ old('username') }}" required autofocus>
-      @if ($errors->has('username'))
-        <span class="invalid-feedback">
-          {{ $errors->first('username') }}
-        </span>
-      @endif
-    </div>
+<div class="auth-container">
+  <div class="auth-form-container">
+    <span class="lead fs-3 fw-bold">Welcome back</span>
+    <form class="auth-form" method="POST" action="{{ route('login') }}">
+      {{ csrf_field() }}
+      <!-- Username -->
+      <div class="input-group mb-3">
+        <label for="username" class="form-label"><i class="bi bi-person-fill"></i> <b>Username</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="username" type="text" class="form-control" name="username" placeholder="Enter your username..." value="{{ old('username') }}" required autofocus>
+          @if ($errors->has('username'))
+            <span class="invalid-feedback">
+              {{ $errors->first('username') }}
+            </span>
+          @endif
+        </div>
+      </div>
+      <!-- Password -->
+      <div class="input-group mb-3">
+        <label for="password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Password</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="password" type="password" class="form-control" name="password" placeholder="Enter password..." required>
+          @if ($errors->has('password'))
+            <span class="invalid-feedback">
+              {{ $errors->first('password') }}
+            </span>
+          @endif
+        </div>
+      </div>
+      <!-- remember me check -->
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="remember_me_check">
+        <label class="form-check-label" for="remember_me_check">Remember me</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Log in</button>
+    </form>
   </div>
-  <!-- Password -->
-  <div class="input-group mb-3">
-    <label for="password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Password</b> <span class="text-danger">*</span></label>
-    <div class="input-group has-validation">
-      <input id="password" type="password" class="form-control" name="password" placeholder="Enter password..." required>
-      @if ($errors->has('password'))
-        <span class="invalid-feedback">
-          {{ $errors->first('password') }}
-        </span>
-      @endif
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Log in</button>
-</form>
+</div>
 <script src="/js/form-validation.js"></script>
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,52 +3,59 @@
 @section('title', 'Register')
 
 @section('content')
-<form class="auth-form" method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
-    <!-- Email -->
-    <div class="input-group mb-3">
-      <label for="email" class="form-label"><i class="bi bi-envelope-fill"></i> <b>Email address</b> <span class="text-danger">*</span></label>
-      <div class="input-group has-validation">
-        <input id="email" type="email" class="form-control" name="email" placeholder="Enter email... (e.g.: email@example.com)"value="{{ old('email') }}" required autofocus>
-        @if ($errors->has('email'))
-          <span class="invalid-feedback">
-            {{ $errors->first('email') }}
-          </span>
-        @endif
+<div class="auth-container">
+  <div class="auth-form-container">
+    <span class="lead fs-3 fw-bold">Welcome to Segmentation fault</span>
+
+    <form class="auth-form" method="POST" action="{{ route('register') }}">
+      {{ csrf_field() }}
+      <!-- Email -->
+      <div class="input-group mb-3">
+        <label for="email" class="form-label"><i class="bi bi-envelope-fill"></i> <b>Email address</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="email" type="email" class="form-control" name="email" placeholder="Enter email... (e.g.: email@example.com)"value="{{ old('email') }}" required autofocus>
+          @if ($errors->has('email'))
+            <span class="invalid-feedback">
+              {{ $errors->first('email') }}
+            </span>
+          @endif
+        </div>
       </div>
-    </div>
-    <!-- Username -->
-    <div class="input-group mb-3">
-      <label for="username" class="form-label"><i class="bi bi-person-fill"></i> <b>Username</b> <span class="text-danger">*</span></label>
-      <div class="input-group has-validation">
-        <input id="username" type="text" class="form-control" name="username" placeholder="Enter username..." value="{{ old('username') }}" required>
-        @if ($errors->has('username'))
-          <span class="invalid-feedback">
-            {{ $errors->first('username') }}
-          </span>
-        @endif
+      <!-- Username -->
+      <div class="input-group mb-3">
+        <label for="username" class="form-label"><i class="bi bi-person-fill"></i> <b>Username</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="username" type="text" class="form-control" name="username" placeholder="Enter username..." value="{{ old('username') }}" required>
+          @if ($errors->has('username'))
+            <span class="invalid-feedback">
+              {{ $errors->first('username') }}
+            </span>
+          @endif
+        </div>
       </div>
-    </div>
-    <!-- Password -->
-    <div class="input-group mb-3">
-      <label for="password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Password</b> <span class="text-danger">*</span></label>
-      <div class="input-group has-validation">
-        <input id="password" type="password" class="form-control" name="password" placeholder="Enter password..." required>
-        @if ($errors->has('password'))
-          <span class="invalid-feedback">
-            {{ $errors->first('password') }}
-          </span>
-        @endif
+      <!-- Password -->
+      <div class="input-group mb-3">
+        <label for="password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Password</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="password" type="password" class="form-control" name="password" placeholder="Enter password..." required>
+          @if ($errors->has('password'))
+            <span class="invalid-feedback">
+              {{ $errors->first('password') }}
+            </span>
+          @endif
+        </div>
       </div>
-    </div>
-    <!-- Repeated Password -->
-    <div class="input-group">
-      <label for="repeated_password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Repeat Password</b> <span class="text-danger">*</span></label>
-      <div class="input-group has-validation">
-        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Repeat password..." required>
+      <!-- Repeated Password -->
+      <div class="input-group mb-3">
+        <label for="repeated_password" class="form-label"><i class="bi bi-lock-fill"></i> <b>Repeat Password</b> <span class="text-danger">*</span></label>
+        <div class="input-group has-validation">
+          <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Repeat password..." required>
+        </div>
       </div>
-    </div>
-    <button type="submit" class="btn btn-primary">Signup</button>
-</form>
+      <!-- submit button -->
+      <button type="submit" class="btn btn-primary mt-2">Signup</button>
+    </form>
+  </div>
+</div>
 <script src="/js/form-validation.js"></script>
 @stop
