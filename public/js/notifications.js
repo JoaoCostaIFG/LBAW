93a6db3a61253b83c66ae6a63ef58747b029cc94
ricardofontao2000 <up201806317@fe.up.pt>
2021-05-08 22:09:58 +0100

    Notifications implemented but returning error

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -9,32 +9,55 @@ let active = false;
 
 // Fill toasts instances array
 notifications.forEach(function (toastNode) {
-  var toast = new bootstrap.Toast(toastNode, {
-    autohide: false
-  });
-  toastsInstances.push(toast);
+    var toast = new bootstrap.Toast(toastNode, {
+        autohide: false
+    });
+    toastsInstances.push(toast);
 });
 
 // Hide / Show notifications on button click
 if (toastBtn != null) {
-  toastBtn.addEventListener("click", () => {
-    if (active) {
-      active = false;
-      toastsInstances.forEach(function (toast) {
-        toast.hide();
-      });
-    } else {
-      active = true;
-      toastsInstances.forEach(function (toast) {
-        toast.show();
-      });
-    }
-  });
+    toastBtn.addEventListener("click", () => {
+        if (active) {
+            active = false;
+            toastsInstances.forEach(function (toast) {
+                toast.hide();
+            });
+        } else {
+            active = true;
+            toastsInstances.forEach(function (toast) {
+                toast.show();
+            });
+        }
+    });
 }
 
-if (element.addEventListener) {
-    element.addEventListener("submit", function(evt) {
-        evt.preventDefault();
-        window.history.back();
-    }, true);
+
+let notification_forms = document.querySelectorAll(".notification-form");
+notification_forms.forEach(notification_form => {
+    if (notification_form.addEventListener) {
+        notification_form.addEventListener("submit", function (evt) {
+            console.log("Hello");
+            evt.preventDefault();
+
+            let xhttp = new XMLHttpRequest();
+            xhttp.open("delete", "/api/notifications/" + notification_form.getAttribute('id').split("-")[1], true);
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhttp.setRequestHeader("X-CSRF-TOKEN", document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+            xhttp.send(
+                encodeForAjax({
+                    csrf: document
+                        .querySelector("meta[name='csrf-token']")
+                        .getAttribute("content"),
+                })
+            );
+        }, true);
+    }
+});
+
+
+function encodeForAjax(data) {
+    return Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&');
 }
--- resources/views/partials/sidebar/notification.blade.php
+++ resources/views/partials/sidebar/notification.blade.php
@@ -9,8 +9,18 @@
     if($time_diff->days) {
         $interval .= $time_diff->days . " days ";
     }
-    $interval .= $time_diff->h . " hours ago"
+    $interval .= $time_diff->h . " hours ago";
 
+    $notification_url = "";
+    if($notification->notification_achievement) {
+        $notification_url = "/profile/" . Auth::id() . "#achievements";
+    } else {
+        if ($notification->notification_post->question) {
+            $notification_url = "/question/" . $notification->notification_post->question->id;
+        } else {
+            $notification_url = "/question/" . $notification->notification_post->answer->question->id;
+        }
+    }
 @endphp
 
 <div class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
@@ -19,22 +29,24 @@
       <strong class="me-auto">{{ $notification->title }}</strong>
       <small>{{ $interval }}</small>
       {{-- Button to delete notifications--}}
-      <form method="POST" class="col-md text-center" action="{{url("api/notifications/" . $notification->id)}}">
+      <form method="POST" id="notification-{{$notification->id}}" class="col-md text-center notification-form" action="{{url("api/notifications/" . $notification->id)}}">
         @csrf
         @method('DELETE')
         <button type="submit" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
       </form>
     </div>
-    <div class="toast-body">
-        {{ $notification->body }}
-        @if ($notification->notification_achievement)
-            {{ $notification->notification_achievement->achievement->body }}
-        @else
-            @if ($notification->notification_post->question)
-                {{ Str::limit($notification->notification_post->question->post->body, 25) }}
+    <a href="{{ url($notification_url) }}">
+        <div class="toast-body">
+            {{ $notification->body }}
+            @if ($notification->notification_achievement)
+                {{ $notification->notification_achievement->achievement->body }}
             @else
-                {{ Str::limit($notification->notification_post->answer->post->body, 25) }}
+                @if ($notification->notification_post->question)
+                    {{ Str::limit($notification->notification_post->question->post->body, 25) }}
+                @else
+                    {{ Str::limit($notification->notification_post->answer->post->body, 25) }}
+                @endif
             @endif
-        @endif
-    </div>
+        </div>
+    </a>
 </div>
