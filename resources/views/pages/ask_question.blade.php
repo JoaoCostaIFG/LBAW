cb8a4698d52b093e6eb5d7b588d2decd6a36c4b0
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-10 17:27:50 +0100

    Update: question ask form.

--- resources/views/pages/ask_question.blade.php
+++ resources/views/pages/ask_question.blade.php
@@ -10,49 +10,47 @@
 <hr class="mt-3">
 <form class="auth-form" method="POST" action="{{ route('ask') }}">
   @csrf
-  <div class="container py-1">
-    <div class="col-auto input-group">
-      <label for="title" class="form-label"><b>Question Title</b><span class="text-danger"> *</span></label>
-      <div class="input-group has-validation">
-        <input type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}"
-          value="{{ old('title') }}" id="title" placeholder="Enter question title... (ex: How to sort an array in c?)" required>
-        @if ($errors->has('title'))
-        <div class="invalid-feedback">
-            {{ $errors->first('title') }}
-        </div>
-        @endif
+  <div class="input-group">
+    <label for="title" class="form-label"><b>Question Title</b><span class="text-danger"> *</span></label>
+    <div class="input-group has-validation">
+      <input type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}"
+        value="{{ old('title') }}" id="title" placeholder="Enter question title... (e.g.: How to sort an array in c?)" required>
+      @if ($errors->has('title'))
+      <div class="invalid-feedback">
+          {{ $errors->first('title') }}
       </div>
-    </div>
-
-    <div class="col-auto input-group py-2">
-      <label for="question-body" class="form-label"><b>Question details</b><span class="text-danger"> *</span></label>
-      <div class="input-group has-validation">
-        <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" id="body" name="body" placeholder="Enter question details..." rows=10>{{ old('body') }}</textarea>
-        @if ($errors->has('body'))
-        <div class="invalid-feedback">
-          {{ $errors->first('body') }}
-        </div>
-        @endif
-      </div>
+      @endif
     </div>
   </div>
 
-  <div class="container-fluid row input-group justify-content-center py-2 my-3">
-    <div class="col-2 col-lg-3 card p-1">
+  <div class="input-group mt-2">
+    <label for="question-body" class="form-label"><b>Question details</b><span class="text-danger"> *</span></label>
+    <div class="input-group has-validation">
+      <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" id="body" name="body" placeholder="Enter question details..." rows=10>{{ old('body') }}</textarea>
+      @if ($errors->has('body'))
+      <div class="invalid-feedback">
+        {{ $errors->first('body') }}
+      </div>
+      @endif
+    </div>
+  </div>
+
+  <div class="row input-group justify-content-center my-3 gx-0">
+    <div id="bounty-slider" class="col-12 col-md-4 col-lg-3 card p-1">
       <div class="card-body col-auto">
         <span class="card-title"><b>Bounty</b></span>
         <hr class="my-1">
-        <div class="text-center my-1">
+        <div class="text-center mt-1">
           <label for="bounty" class="form-labelr" id="bountyValue">{{ old('bounty', 0) }}</label>
           <input type="range" name="bounty" class="form-range" value="{{ old('bounty', 0) }}" min="0" max="100" step="1" id="bounty" onmousemove="document.getElementById('bountyValue').innerText = document.getElementById('bounty').value">
         </div>
       </div>
     </div>
-    <div class="col-3 col-md-4 col-lg-6 card p-1">
+    <div id="topic-input" class="col-12 col-md-8 col-lg-6 card p-1">
       <div class="card-body">
         <span class="card-title"><b>Topics</b></span>
         <hr class="my-1">
-        <div class="mt-3 has-validation">
+        <div class="has-validation mt-3">
           <select class="js-example-basic-single form-control {{ $errors->has('topics') ? 'is-invalid' : ''}}"
              name="topics[]" value="{{ old('topics') }}" rows=2 id="topics" placeholder="Enter topic tags" multiple required>
              @foreach ($topics as $topic)
