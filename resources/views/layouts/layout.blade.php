328b7aae37c5d30d296bced5b9a5912c4a664cb4
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-11 11:36:37 +0100

    Update: minified images further.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -92,11 +92,8 @@
     </main>
 
     <!-- Footer -->
-    <a class="btn btn-secondary" id="go-up-button" href="#" role="button">
-      <i class="bi bi-arrow-up"></i>
-    </a>
-
     <footer class="container">
+      <a class="btn btn-secondary" id="go-up-button" href="#" role="button"><i class="bi bi-arrow-up"></i></a>
       <br>
       <hr>
       <div class="row justify-content-between">
@@ -104,7 +101,6 @@
         <a class="col-auto text-end" href="#">go to top</a>
       </div>
     </footer>
-
     <!-- Bootstrap + Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script>
