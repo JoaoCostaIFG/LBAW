0ef9dfebf9464c139cd657960fcdeefadf8be990
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-12 19:25:47 +0100

    WIP: question's page rework.

--- resources/views/partials/posts/comment_block.blade.php
+++ resources/views/partials/posts/comment_block.blade.php
@@ -1,20 +1,26 @@
 <div class="col-9 offset-3 col-md-10 offset-md-2 col-lg-11 offset-lg-1">
-    @each('partials.posts.comment', $post->comments, 'comment')
-    @auth
-        <div id="end-comment-{{$post->id}}" class="row">
-            <a class="text-center dropdown-toggle" data-bs-toggle="dropdown"
-            id="commentDropdownLink" aria-expanded="false">
-                Add comment
-            </a>
-            <div class="dropdown-menu" aria-labelledby="commentDropdownLink">
-                <form class="container">
-                    <h2>Your Comment</h2>
-                    <textarea class="form-control" id="Textarea{{$post->id}}" rows="7"></textarea>
-                    <label class="visually-hidden" for="Textarea{{$post->id}}">Add comment</label>
-                    <a class="btn btn-primary" onclick="addComment({{$answer_id ?? 'null'}}, {{$question_id ?? 'null'}})">Post Comment</a>
-                </form>
-            </div>
-            <script src="/js/add_comment.js"></script>
-        </div>
-    @endauth
+  @each('partials.posts.comment', $post->comments, 'comment')
+  @auth
+    <div id="end-comment-{{$post->id}}" class="row gx-0 justify-content-center mt-1">
+      <button id="commentDropdownLink" class="col-auto btn btn-outline-success text-center dropdown-toggle"
+       data-bs-toggle="dropdown" aria-expanded="false">
+        Add comment
+      </button>
+      <div class="col-12 dropdown-menu" aria-labelledby="commentDropdownLink">
+        <form class="container">
+          <h4>Your Comment</h4>
+          <textarea class="form-control" id="Textarea{{$post->id}}" rows="7"></textarea>
+          <label class="visually-hidden" for="Textarea{{$post->id}}">Add comment</label>
+          <br>
+          <div class="text-end">
+            <button class="btn btn-primary" type="submit"
+             onclick="addComment({{$answer_id ?? 'null'}}, {{$question_id ?? 'null'}})">
+              Post Comment
+            </button>
+          </div>
+        </form>
+      </div>
+      <script src="/js/add_comment.js"></script>
+    </div>
+  @endauth
 </div>
--- resources/views/partials/posts/vote.blade.php
+++ resources/views/partials/posts/vote.blade.php
@@ -14,16 +14,18 @@ if ($vote)
 
 @endphp
 <span class="row fs-4 justify-content-center text-center">
-  <button id="btn-upvote-{{$post->id}}" @if($can_vote) onclick="updateVote({{$post->id}}, true)" @endif
-   class="col-12 btn p-0 {{ $can_vote ? 'text-success cursor-na' : ''}}"
-   style="{{ $can_vote ? '' : 'cursor: not-allowed'}}">
-    <i class="bi bi-caret-up{{$up_text}}"></i>
-  </button>
+  <span class="col-12">
+    <button id="btn-upvote-{{$post->id}}" @if($can_vote) onclick="updateVote({{$post->id}}, true)" @endif
+     class="btn p-0 bi bi-caret-up{{$up_text}} {{ $can_vote ? 'text-success cursor-na vote-up-btn' : ''}}"
+     style="{{ $can_vote ? '' : 'cursor: not-allowed'}}">
+    </button>
+  </span>
   <span class="col-12" id="score-{{$post->id}}">{{ $post->score }}</span>
-  <button id="btn-downvote-{{$post->id}}" @if($can_vote) onclick="updateVote({{$post->id}}, false)" @endif
-   class="col-12 btn p-0 {{ $can_vote ? 'text-danger cursor-na' : ''}}"
-   style="{{ $can_vote ? '' : 'cursor: not-allowed'}}">
-    <i class="bi bi-caret-down{{$down_text}}"></i>
-  </button>
+  <span class="col-12">
+    <button id="btn-downvote-{{$post->id}}" @if($can_vote) onclick="updateVote({{$post->id}}, false)" @endif
+     class="btn p-0 bi bi-caret-down{{$down_text}} {{ $can_vote ? 'text-danger cursor-na vote-down-btn' : ''}}"
+     style="{{ $can_vote ? '' : 'cursor: not-allowed'}}">
+    </button>
+  </span>
 </span>
 <script src="/js/add_vote.js"></script>
