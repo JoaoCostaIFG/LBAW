f35b8fce626f2814d0162eaaefe55f40a473ba8c
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-14 15:57:31 +0100

    Fix destino de corrida (meta).

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="Assembling a community-driven database of knowledge by asking and answering questions.">
+    <meta name="description" content="Assembling a community-driven database of knowledge by asking and answering questions.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
