3980d768d8dd20e776e892468152f63c9cc7309f
Ana Ines <anaiobarros2014@gmail.com>
2021-05-14 17:09:33 +0100

    Fix: notification css

--- resources/views/partials/sidebar/notification.blade.php
+++ resources/views/partials/sidebar/notification.blade.php
@@ -1,40 +1,41 @@
 @php
-    $notification_url = "";
-    if($notification->notification_achievement) {
-        $notification_url = "/profile/" . Auth::id() . "#achievements";
+$notification_url = '';
+if ($notification->notification_achievement) {
+    $notification_url = '/profile/' . Auth::id() . '#achievements';
+} else {
+    if ($notification->notification_post->question) {
+        $notification_url = '/question/' . $notification->notification_post->question->id;
     } else {
-        if ($notification->notification_post->question) {
-            $notification_url = "/question/" . $notification->notification_post->question->id;
-        } else {
-            $notification_url = "/question/" . $notification->notification_post->answer->question->id;
-        }
+        $notification_url = '/question/' . $notification->notification_post->answer->question->id;
     }
+}
 @endphp
 
-<div class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-      <i class="bi bi-exclamation-circle-fill p-1"></i>
-      <strong class="me-auto">{{ $notification->title }}</strong>
-      <small>{{ (new \Carbon\Carbon($notification->date))->diffForHumans() }}</small>
-      {{-- Button to delete notifications--}}
-      <form method="POST" id="notification-{{$notification->id}}" class="col-md text-center notification-form" action="{{url("api/notifications/" . $notification->id)}}">
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <i class="bi bi-exclamation-circle-fill p-1"></i>
+    <strong class="me-auto">{{ $notification->title }}</strong>
+    <small class="text-muted">{{ (new \Carbon\Carbon($notification->date))->diffForHumans() }}</small>
+    <form method="POST" id="notification-{{$notification->id}}" class="d-flex notification-form" action="{{url("api/notifications/" . $notification->id)}}">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </form>
-    </div>
+        <button type="button" class="btn-close p-0 me-1" data-bs-dismiss="toast" aria-label="Close"></button>
+    </form> 
+  </div>
+  <div class="toast-body text-break px-1">
     <a href="{{ url($notification_url) }}">
-        <div class="toast-body">
-            {{ $notification->body }}
-            @if ($notification->notification_achievement)
-                {{ $notification->notification_achievement->achievement->body }}
-            @else
-                @if ($notification->notification_post->question)
-                    {{ Str::limit($notification->notification_post->question->post->body, 25) }}
-                @else
-                    {{ Str::limit($notification->notification_post->answer->post->body, 25) }}
-                @endif
-            @endif
-        </div>
+      {{ $notification->body }}
+      @if ($notification->notification_achievement)
+        {{ $notification->notification_achievement->achievement->body }}
+      @else
+        @if ($notification->notification_post->question)
+          {{ Str::limit($notification->notification_post->question->post->body, 70) }}
+        @else
+          {{ Str::limit($notification->notification_post->answer->post->body, 70) }}
+        @endif
+      @endif
     </a>
+  </div>
 </div>
+
+
