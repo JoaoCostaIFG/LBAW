c16d0d854d5c4d2331f2de5ac03940130bb4ccd8
Ana Ines <anaiobarros2014@gmail.com>
2021-05-20 22:12:42 +0100

    Add: validation edit comment

--- public/js/edit_comment_answer.js
+++ public/js/edit_comment_answer.js
@@ -1,26 +1,26 @@
 'use strict'
 
-function editComment(comment_id) {
+function editCommentAnswer(id, type) {
 
-    let new_value = document.getElementById("Textarea-edit-" + comment_id).value;
+    let new_value = document.getElementById("Textarea-edit-" + type + '-' + id).value;
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-            let comment = document.getElementById("comment-" + comment_id);
+            let comment = document.getElementById(type + '-' + id);
             if (this.status == 200) {
                 comment.textContent = new_value;
             } else {
-                comment.textContent = "Something went wrong editing this comment. Please try again.";
+                comment.textContent = "Something went wrong while editing. Please try again.";
                 comment.classList.add("text-danger");
             }
         }
     };
 
-    xhttp.open("PATCH", "/api/comments/" + comment_id, true);
+    xhttp.open("PATCH", "/api/comments/" + id, true);
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.setRequestHeader("X-CSRF-TOKEN", document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
     xhttp.send(encodeForAjax({
-        comment_id: comment_id,
+        id: id,
         body: new_value,
         csrf: document
             .querySelector("meta[name='csrf-token']")
