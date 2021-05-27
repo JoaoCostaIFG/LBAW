2c171b2c884574edf64a8e29901caae19461f886
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-05-27 21:30:19 +0100

    Fix: double class= is no bueno.

--- resources/views/partials/posts/answer.blade.php
+++ resources/views/partials/posts/answer.blade.php
@@ -9,12 +9,11 @@
   <div class="col-10 col-sm-11 row justify-content-end mb-1 ms-1 pb-2 border-start border-dark">
     <!-- answer body -->
     <p id="answer-{{$answer->post->id}}" class="col-12 text-break ps-0 pe-45 answer-body">{{ $answer->post->body }} </p>
-
     @include('partials.posts.user_card', ['post' => $answer->post])
 
     <!-- Options -->
     @auth
-      <div class="answer-options" class="col-auto">
+      <div class="answer-options col-auto">
         <!--Check if is moderator-->
         <ul class="nav nav-pills">
           <li class="nav-item dropdown ms-auto">
