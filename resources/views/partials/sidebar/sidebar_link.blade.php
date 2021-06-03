1013dbd1b427d1c5ba423f7b368090376a77cdf8
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-06-03 17:59:59 +0100

    WIP: filters to minify html

--- resources/views/partials/sidebar/sidebar_link.blade.php
+++ resources/views/partials/sidebar/sidebar_link.blade.php
@@ -1,16 +1,5 @@
-<li class="list-group-item
-@if ($is_selected)
-    ' active'
-@endif
-">
-    <a href= {{ $href }}>
-      <i class="{{ 'bi ' . $icon }}"></i>
-        @if ($is_selected)
-            <b>
-        @endif
-        {{ " " . $text }}
-        @if ($is_selected)
-            </b>
-        @endif
-    </a>
-  </li>
+<li class="list-group-item @if ($is_selected)'active'@endif">
+  <a href={{ $href }}>
+    <i class="{{ 'bi ' . $icon }}"></i>@if ($is_selected)<b>{{ $text }}</b>@else{{ $text }}@endif
+  </a>
+</li>
